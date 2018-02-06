2ff2f8ad52c3dcce2784ef405f3f502c259582b5
John Wu <john.wu@mail.mcgill.ca>
2018-02-06 01:10:13 -0500

    Created bash script for building environment

---
+++ build_environment.sh
@@ -0,0 +1,7 @@
+sudo apt-get update
+curl -sL https://deb.nodesource.com/setup_8.x | sudo -E bash -
+sudo apt-get -y install nodejs 
+sudo apt-get install -y build-essential
+sudo apt-get clean
+sudo npm install -g cordova ionic 
+sudo npm install --no-bin-links
