83e7b0d43835ca263e2cf28ad8ced5c5b3dc485d
John Wu <john.wu@mail.mcgill.ca>
2018-02-08 01:07:13 -0500

    Trying to fix this travis ci shit

--- build_environment.sh
+++ build_environment.sh
@@ -1,5 +1,5 @@
 sudo apt-get update
-curl -sL https://deb.nodesource.com/setup_8.x | sudo -E bash -
+curl -sL https://deb.nodesource.com/setup_9.x | sudo -E bash -
 sudo apt-get -y install nodejs 
 sudo apt-get install -y gcc g++ build-essential
 sudo apt-get clean
