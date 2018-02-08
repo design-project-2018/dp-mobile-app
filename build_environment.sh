86b6507a1276eae2f0d5581192f5c434234c560a
John Wu <john.wu@mail.mcgill.ca>
2018-02-08 16:11:10 -0500

    Come on please work this time

--- build_environment.sh
+++ build_environment.sh
@@ -7,8 +7,10 @@ sudo mkdir ~/.npm-global
 sudo npm config set prefix '~/.npm-global'
 sudo export PATH=~/.npm-global/bin:$PATH
 sudo source ~/.profile
-sudo npm install npm@latest -g
+sudo npm config set user 0
+sudo npm config set unsafe-perm true
+sudo npm install -g npm@latest
 sudo rm -rf node_modules
 sudo npm cache -f clean
 sudo npm install -g cordova ionic 
-sudo npm install --no-bin-links --unsafe-perm
+sudo npm install --no-bin-links 
