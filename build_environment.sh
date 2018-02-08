edda0f50247ee01515a60239faf2a6ce7e74a9b2
John Wu <john.wu@mail.mcgill.ca>
2018-02-08 01:26:55 -0500

    Quitting for the night, will fix this tomorrow

--- build_environment.sh
+++ build_environment.sh
@@ -3,7 +3,5 @@ curl -sL https://deb.nodesource.com/setup_9.x | sudo -E bash -
 sudo apt-get -y install nodejs 
 sudo apt-get install -y gcc g++ build-essential
 sudo apt-get clean
-NPM_CONFIG_PREFIX=~/.npm-global
-source ~/.profile
 npm install -g cordova ionic 
 npm install --no-bin-links --unsafe-perm
