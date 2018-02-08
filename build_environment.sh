e9b6c9579563fae565070cb4ee488cfb5a3a2d2c
John Wu <john.wu@mail.mcgill.ca>
2018-02-08 15:29:27 -0500

    Come on please work this time

--- build_environment.sh
+++ build_environment.sh
@@ -3,6 +3,10 @@ curl -sL https://deb.nodesource.com/setup_8.x | sudo -E bash -
 sudo apt-get -y install nodejs 
 sudo apt-get install -y gcc g++ build-essential
 sudo apt-get clean
+sudo mkdir ~/.npm-global
+sudo npm config set prefix '~/.npm-global'
+sudo export PATH=~/.npm-global/bin:$PATH
+sudo source ~/.profile
 sudo npm install npm@latest -g
 sudo rm -rf node_modules
 sudo npm cache -f clean
