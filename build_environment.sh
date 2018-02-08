dfd3b0c650206516c80c3ca23e1d34f86a7333be
John Wu <john.wu@mail.mcgill.ca>
2018-02-08 14:53:43 -0500

    Come on please work this time

--- build_environment.sh
+++ build_environment.sh
@@ -1,10 +1,10 @@
 sudo apt-get update
-curl -sL https://deb.nodesource.com/setup_9.x | sudo -E bash -
+curl -sL https://deb.nodesource.com/setup_8.x | sudo -E bash -
 sudo apt-get -y install nodejs 
 sudo apt-get install -y gcc g++ build-essential
 sudo apt-get clean
 sudo rm -rf node_modules
-sudo npm cache clean
+sudo npm cache -f clean
 sudo ln -s /usr/bin/nodejs /usr/bin/node
 sudo npm install -g cordova ionic 
 sudo npm install --no-bin-links --unsafe-perm
