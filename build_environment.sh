58b4ab3ed21d786dc33d548e63002c04c92c6b07
John Wu <john.wu@mail.mcgill.ca>
2018-02-08 15:15:10 -0500

    Come on please work this time

--- build_environment.sh
+++ build_environment.sh
@@ -3,6 +3,7 @@ curl -sL https://deb.nodesource.com/setup_8.x | sudo -E bash -
 sudo apt-get -y install nodejs 
 sudo apt-get install -y gcc g++ build-essential
 sudo apt-get clean
+sudo npm install npm@latest -g
 sudo rm -rf node_modules
 sudo npm cache -f clean
 sudo ln -sf /usr/bin/nodejs /usr/bin/node
